3808228650ca50f73f12e63773ab83d181798217
rgb <ruben.gbarriada@gmail.com>
2022-04-22 07:58:12 +0200

    Initial commit

---
+++ dr_main.py
@@ -0,0 +1,16 @@
+"""
+# Author: ruben 
+# Date: 22/4/22
+# Project: DRPrediction
+# File: dr_main.py
+
+Description: "Enter feature description here"
+"""
+
+
+def main():
+    print("run")
+
+
+if __name__ == '__main__':
+    main()
